6fda0ebe94f397b575ccb30550e7221997d4a5fa
oluyomi justina <justinaolamide2611@gmail.com>
2023-08-22 15:09:41 +0100

    fix

---
+++ index.js
@@ -0,0 +1,7 @@
+let menu =document.querySelector('#menu');
+let nav =document.querySelector('nav ul');
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle(".open");
+}
